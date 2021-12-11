bb65365eb4be2e64f9355803acc70d2573dfcbd8
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-12-11 15:27:55 +0600

    authentication middleware added

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const categoryController = require('../category/category.controller');
+const { isAuthenticated } = require('../user/user.controller');
 
-router.post('/category', categoryController.createCategory)
-router.put('/category/:id', categoryController.updateCategory)
-router.delete('/category/:id', categoryController.deleteCategory)
+router.post('/category', isAuthenticated, categoryController.createCategory)
+router.put('/category/:id', isAuthenticated, categoryController.updateCategory)
+router.delete('/category/:id', isAuthenticated, categoryController.deleteCategory)
 router.get('/categories', categoryController.getCategories)
 router.get('/category/:id', categoryController.getCategory)
 // router.get('/categories', categoryController.getCategoriesByQuery)
